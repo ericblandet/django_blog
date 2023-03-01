c3889020fc54528c130bfdde0b82c3b584937efc
eric <eric.blandet@gmail.com>
2023-03-01 07:46:29 +0100

    home on root url

--- src/django_blog/urls.py
+++ src/django_blog/urls.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
-    path('blog/', include("posts.urls")),
+    path('', include("posts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
